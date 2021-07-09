d52e031afc35f0d74b01ff4224a2a42effdd4272
Soumyanath Datta <soumyanath.datta@pwc.com>
2021-07-09 13:09:25 +0530

    fix sorting of euclidean distances

--- KNN_classifier.py
+++ KNN_classifier.py
@@ -3,11 +3,11 @@ from pandas.core.frame import DataFrame
 
 class Knn_Classifier:  
     training_set: DataFrame
-    k_val:int = 0
+    k_val:int = 0    
 
-    def __init__(self, x_train, k:int):
-        self.training_set = x_train
-        self.k_val = k 
+    def __init__(self, training_set, k:int):
+        self.training_set = training_set
+        self.k_val = k         
     
     @staticmethod
     def get_distance(row1, row2):
@@ -18,12 +18,16 @@ class Knn_Classifier:
         for index, row in self.training_set.iterrows():
             distance = self.get_distance(input[0], row[0:8])
             row_dist[index] = distance
-        idxs = sorted(row_dist, key=row_dist.get) 
-        return row_dist, idxs
+        idxs = sorted(row_dist.items(), key= lambda kv:(kv[1])) 
+        return idxs
 
-    def predict(self, input):                
-        distances, sorted_idxs = self.get_neighbours(input)
-        print(sorted_idxs[0:self.k_val])
-        possible_outcomes = self.training_set.iloc[sorted_idxs[0:self.k_val]]
+    def predict(self, input):   
+        idxs = []             
+        sorted_idxs = self.get_neighbours(input)
+        # Get k indices
+        for idx, _ in sorted_idxs[0:self.k_val]:
+            idxs.append(idx)
+        print(self.training_set['Outcome'].index)
+        possible_outcomes = self.training_set['Outcome'].iloc[idxs]
         winning_class = max(set(possible_outcomes), key=possible_outcomes.count)
         return winning_class
