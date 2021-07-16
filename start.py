40ef5ecb35fc52c9947967f1359d6c77a55b7161
tito-datta <tito.datta@gmail.com>
2021-07-16 15:05:47 +0530

    add performance hsiistory

--- start.py
+++ start.py
@@ -13,13 +13,19 @@ X_Train,X_Test,Y_Train,Y_Test = train_test_split(predictors, target, test_size=0
 diabetes_train = pd.DataFrame(data=X_Train, columns=predictors.columns).assign(Outcome=Y_Train)
 diabetes_test = pd.DataFrame(data=X_Test, columns=predictors.columns).assign(Outcome=Y_Test)
 
-for i in range(25,50):
+performance_hist = {}
+for i in range(1,30):
     knn = knn_classifier(diabetes_train,i,'Outcome')
     predicted = []
     for row in diabetes_test.itertuples():
         predicted.append(knn.predict(row[0:8]))
 
     accuracy = metrics.accuracy_score(Y_Test, predicted)
-    print(f'The accuracy of the model for k = {i} is {round(accuracy, 4)}.')
+    f1_score = metrics.f1_score(Y_Test, predicted)
+    performance_hist[i] = (round(accuracy, 4),round(f1_score, 4))
+    # print(f'The accuracy of the model for k = {i} is {round(accuracy, 4)} and the f1 score of the model for k = {i} is {round(f1_score, 4)}.')
 
-
+sorted_idxs = sorted(performance_hist, key=performance_hist.get, reverse=True)
+print('The top three performing values of K are: ')
+for idx in sorted_idxs[0:3]:
+    print(f'For value {idx} of k the accuracy is {performance_hist[idx][0]} and the f1 score is {performance_hist[idx][1]}')
